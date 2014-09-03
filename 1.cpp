b75bd2b97fc98522cd8672a7165a5911e36f7b27
stanley zheng <dev.stanzheng@gmail.com>
2014-09-03 15:08:42 -0400

    init commit

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	std::cout << 1/6 << "\n";
+	return 0;
+}
+
+/*
+3) (40 points) Write a program that receives an integer number in binary format and convert it to
+decimal. You MUST use the algorithm described in the class
+*/
+/*
+4) (5 points) This line of code is written in C++.
+std::cout << 1/6 << ”\n” ;
+
+Output is 0 with remainder 1.
+
+*/
